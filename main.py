eb21f22c68782cae962725ba404c0b314ce8166a
ruslanaseverova <ruslanaseverova543@gmail.com>
2026-01-11 10:15:08 +0300

    с см

--- main.py
+++ main.py
@@ -10,28 +10,28 @@ def index():
     if request.method == 'POST':
         # получаем выбранное изображение
         selected_image = request.form.get('image-selector')
-
         # Задание №2.Получаем текст
-        
+        text1 = request.form.get('textTop')
+        text2 = request.form.get('textBottom')
 
         # Задание №3. Получаем расположение текста
-       
+        x1 = request.form.get('textTop_y')
+        x2 = request.form.get('textBottom_y')
 
         # Задание №3. Получаем цвет текста
-        
+        color = request.form.get('color-selector')
 
         return render_template('index.html', 
                                # отображаем выбранное изображение
                                selected_image=selected_image, 
-
                                # Задание №2. Отображаем текст
-                               
-
+                               text1 = text1,
+                               text2 = text2,
                                # Задание №3. Отображаем цвет 
-                               
-                               
+                               x1 = x1,
+                               x2 = x2,
                                #Задание №3. Отоброжаем расположение текста
-
+                               color = color,
                                )
     else:
         # отображаем первое изображение по умолчанию
